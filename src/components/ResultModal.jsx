605aa1e5b139cccf6de1305c9cb452a5ccdca003
Kirill Anikin <anikinkirill22@gmail.com>
2022-02-28 22:57:46 +0300

    feat: Copy to clipboard added

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,7 +1,23 @@
+import { set } from "lodash";
+import { useState, useCallback } from "react";
 import { Modal, Button } from "react-bootstrap";
 import { FaCopy } from "react-icons/fa";
 
 const ResultModal = ({ generatedJSON, isOpen, onClose }) => {
+  const [isCopied, setIsCopied] = useState(false);
+
+  const handleCopy = useCallback(() => {
+    navigator.clipboard.writeText(generatedJSON).then(
+      () => {
+        setIsCopied(true);
+        setTimeout(() => setIsCopied(false), 1_000);
+      },
+      () => {
+        setIsCopied(false);
+      }
+    );
+  }, [isCopied]);
+
   return (
     <Modal scrollable show={isOpen} onHide={onClose}>
       <Modal.Header closeButton>
@@ -11,8 +27,14 @@ const ResultModal = ({ generatedJSON, isOpen, onClose }) => {
         <pre>{generatedJSON}</pre>
       </Modal.Body>
       <Modal.Footer>
-        <Button variant="primary" onClick={onClose}>
-          <FaCopy /> Copy
+        <Button variant="primary" onClick={handleCopy}>
+          {!isCopied ? (
+            <>
+              <FaCopy /> Copy
+            </>
+          ) : (
+            "Copied !"
+          )}
         </Button>
         <Button variant="primary" onClick={onClose}>
           Close
